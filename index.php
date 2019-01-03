17cd648be231bb6cad3acc39c67cf491e97d67ad
Ziratsu <enzo.ustariz@hotmail.fr>
2019-01-03 23:15:59 +0100

    init

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111230996-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-111230996-1');
+</script>
+
+        <title>Votre Paysagiste, Contact Audenge et bassin d'arcachon</title>
+        <meta name="description" content="Contact avec Castegnaro-exterieur, entreprise de Paysagiste au bassin d'Arcachon, Biganos, Audenge, Laton." />
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link href="https://fonts.googleapis.com/css?family=Jura" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/script.js"></script>
+ 
+ 
+        
+    </head>
+ <body> <!-- BARRE DE NAVIGATION --> <nav class="navbar navbar-default navbar-fixed-top"> <div class="container"> <div class="navbar-header"> <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> <span class="icon-bar" style="background-color: white;"></span> <span class="icon-bar" style="background-color: white;"></span> <span class="icon-bar" style="background-color: white;"></span> </button> </div> <div class="collapse navbar-collapse" id="myNavbar"> <ul class="nav navbar-nav"> <li><a href="index.html#responsive_img">ACCUEIL</a></li> <li> <div class="dropdown"> <a href="index.html#Services" alt="services" ><button id="togglebtn" class="dropbtn ">SERVICES<span class="caret"></span></button></a> <div class="dropdown-content"> <a id="nav64" href="index.html#services">Élagage</a> <a id="nav64" href="index.html#services">Clôtures /<br>Palissades</a> <a id="nav64" href="index.html#services">Déssouchage</a> <a id="nav64" href="index.html#rowpic1">Entretien Toitures</a> <a id="nav64" href="index.html#rowpic1">Terrasses<br>Cabanon bois</a> <a id="nav64" href="index.html#rowpic1">Plantations</a> <a id="nav64" href="index.html#rowpic2">Terrassement</a> <a id="nav64" href="index.html#rowpic2">Gazon synthétique</a> <a id="nav64" href="index.html#rowpic2">Gazon naturel</a> <a id="nav64" href="index.html#rowpic3">Broyeur de branches</a> <a id="nav64" href="index.html#rowpic3">Forrage de puits</a> <a id="nav64" href="index.html#rowpic3">Taille de haies</a> </div></div></li> <li><a href="index.html#apropos">A PROPOS</a></li> <li><a href="chantiers.html">CHANTIERS</a></li> <li><a href="index.php">Contact</a></li> </ul> </div> </div></nav> <div><div class="container" style="margin-top: 80px;"> <div id="heading"> <h2 id="services2">Contacts</h2> <div class="divider"></div> </div><div class="row"><div class="col-sm-6" style="margin-top:50px;"><p class="contactsfin">Appelez-nous :</p><div><h3 id="txtcontact">Pour toute question, appelez-nous au : 06.58.17.84.64 de 9 à 19 heures.<br>Ou envoyez-nous un message ci-dessous.</h3></div></div><div class="col-sm-6"> <img src="castegnaro.png" class="img-responsive" id="logo"></div></div></div> <div class="container"> <div class="divider"></div> <div class="heading"> <p class="contactsfin">Contactez-moi</p> </div> <div class="row"> <div class="col-lg-10 col-lg-offset-1"> <form id="contact-form" method="post" action="" role="form"> <div class="row"> <div class="col-md-6"> <label for="firstname">Prénom <span class="blue">*</span></label> <input id="firstname" type="text" name="firstname" class="form-control" placeholder="Votre prénom"> <p class="comments"></p> </div> <div class="col-md-6"> <label for="name">Nom <span class="blue">*</span></label> <input id="name" type="text" name="name" class="form-control" placeholder="Votre Nom"> <p class="comments"></p> </div> <div class="col-md-6"> <label for="email">Email <span class="blue">*</span></label> <input id="email" type="text" name="email" class="form-control" placeholder="Votre Email"> <p class="comments"></p> </div> <div class="col-md-6"> <label for="phone">Téléphone</label> <input id="phone" type="tel" name="phone" class="form-control" placeholder="Votre Téléphone"> <p class="comments"></p> </div> <div class="col-md-12"> <label for="message">Message <span class="blue">*</span></label> <textarea id="message" name="message" class="form-control" placeholder="Votre Message" rows="4"></textarea> <p class="comments"></p> </div> <div class="col-md-12"> <p class="blue"><strong>* Ces informations sont requises.</strong></p> </div> <div class="col-md-12"> <input type="submit" class="button1" value="Envoyer"> </div> </div> </form> </div> </div> </div> <footer><div class="container-fluid"> <div id="fintxt" class="col-xs-12"><p> OU SUIS-JE ?<br> AUDENGE, 33980<br> Téléphone: 06.58.17.84.64 </p><div class="logofoot"></div></div></footer> <div id="signature"><a id="mentions" href="Mentions Légales.html">Mentions légales ©</a></div> </body></html>
